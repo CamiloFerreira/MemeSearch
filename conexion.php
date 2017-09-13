7df7b1536a1743a17965bf45c3bedf358e555a46
Unknown <lferreira2017@alu.uct.cl>
2017-09-13 17:04:47 -0300

    Conexion Con base de datos
    Esta version y/o cambio , tiene como principal caracteriztica el guardo de los registros en la base de datos , esto se ve en la parte de registrarse

--- conexion.php
+++ conexion.php
@@ -1,8 +1,36 @@
-<<?php  
- define ("DB_SERVER","localhost");
- define ("DB_NAME","usuarios");
- define ("DB_USER","usuario");
- define ("DB_PASS","clave")
- $con = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
- mysql_select_db(usuarios,$con)
+<?php 
+	//Conectarse con el servidor
+ $conn = mysqli_connect('localhost','root','');
+ if (!$conn)
+ {
+	 die("Conexion Fallada" . mysqli_connect_error());
+ }
+ else
+ {
+	 $base = mysqli_select_db($conn,'memes');
+	 if (!$base)
+	 {
+		 echo "No se Encontro la Base De datos";
+		 
+	 }
+ }
+ $usuario = $_GET['usuarios'];
+ $clave   = $_GET['clave'];
+ $correo  = $_GET['correo'];
+ //Hacer la sentencia de sql
+ $sql = "INSERT INTO usuarios (clave,usuario,correo) VALUES ('$clave',
+ 													  '$usuario',
+                                                      '$correo')";
+ //Ejecutar sentencia sql
+ $ejecutar =mysqli_query($conn,$sql);
+ if (!$ejecutar)
+ {
+	 echo "Se Produjo un error";
+ }
+ else
+ {
+	 echo "Datos Guardados Correctamente <br> <a href='inicio_seccion.html'>Volver </a> ";
+ }
+
+
 ?>
